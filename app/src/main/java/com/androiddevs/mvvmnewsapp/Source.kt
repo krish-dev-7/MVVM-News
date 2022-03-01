9c909413c7a9f226a8ecd33ccefbd4c2c0b3967a
krishna <krish0cyber@gail.com>
2022-03-01 19:11:19 +0530

    up

--- app/src/main/java/com/androiddevs/mvvmnewsapp/Source.kt
+++
@@ -1,9 +0,0 @@
-package com.androiddevs.mvvmnewsapp
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Source(
-    val id: Any,
-    val name: String
-)
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,7 +2,7 @@ package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
 import kotlinx.android.synthetic.main.activity_news.*
